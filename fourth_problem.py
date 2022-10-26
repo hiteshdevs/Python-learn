b1410ae6dbb222f342ec46d9da5f9d68ee73763a
Hitesh Bhagwat <111442899+hiteshdevs@users.noreply.github.com>
2022-10-27 01:01:20 +0530

    Add files via upload

---
+++ fourth_problem.py
@@ -0,0 +1,7 @@
+userName = input('Enter your Name: ')
+userAge = int(input('Enter your age: '))
+
+if userAge >= 18:
+	print('Eligible for Vote')
+else:
+	print('Uneligible for Vote')
---
+++ third_problem.py
@@ -0,0 +1,11 @@
+userName = input('PLease enter your name')
+
+print('Enter your marks according to your subjects')
+
+English = int(input('English : '))
+Science = int(input('Science : '))
+Maths = int(input('Maths : '))
+
+averageOfAllSubjects = (English + Science + Maths) / 3
+
+print(averageOfAllSubjects)
