ca541888146c7cd486f45661d02448a9e2f5cb23
Hitesh Bhagwat <111442899+hiteshdevs@users.noreply.github.com>
2022-10-26 18:30:37 +0530

    Add files via upload

---
+++ first_problem.py
@@ -0,0 +1,13 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+yearOfBirth = input("Please enter your year of birth: ")
+
+isNewUser = True
+
+favourite_food = ['Apple','Kiwi','Mango']
+
+print(name)
+print(age)
+print(yearOfBirth)
+print(isNewUser)
+print(favourite_food)
---
+++ second_problem.py
@@ -0,0 +1,8 @@
+a = int(input("Please enter a: "))
+b = 20
+c = 10
+
+print(type(a))
+a += 10
+
+print((a+b+c)/3)
