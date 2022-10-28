a6edb2f7da07c5283a7f1f7cb7c7ed94df13e18f
Hitesh Bhagwat <111442899+hiteshdevs@users.noreply.github.com>
2022-10-28 18:28:48 +0530

    Add files via upload

---
+++ fivth_problem.py
@@ -0,0 +1,3 @@
+printO = list(range(2 ,20 ,2))
+
+print(printO)
---
+++ sixth_problem.py
@@ -0,0 +1,10 @@
+def cal():
+	my_list = {'1' : 'Hitesh','2' : 'Ekta','3' : 'Harsh','4' : 'Nidish','5' : 'Ashwariya'}
+    my_list[6] = 'Tejas',
+    my_list[7] = 'Trisha'
+    print(my_list['2'])
+    for i in my_list:
+	     print(i, my_list[i])
+
+
+cal()
