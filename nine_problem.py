f2f69afe4e688414ee3039c26fc9b5a03d722a01
Hitesh Bhagwat <111442899+hiteshdevs@users.noreply.github.com>
2022-10-30 10:43:57 +0530

    Add files via upload

---
+++ nine_problem.py
@@ -0,0 +1,8 @@
+def sum_of_list(a):
+  print('Calculating................................................................................................................')
+  return sum(a)
+  
+marks = [45,67,50,19,90,45,89,99]
+sum_of_marks = sum_of_list(marks)
+
+print('sum_of_marks :', sum_of_marks)
